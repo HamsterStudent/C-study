484e0d10a083c073620d677afdd7da3acfb02cbe
HamsterStudent <hwanyamyu@gmail.com>
2023-04-18 14:00:59 +0900

    Graph Traversal

---
+++ algorithm_xcode/hamster/GraphTraversal/GraphTraversal.h
@@ -0,0 +1,11 @@
+#ifndef GraphTraversal_H
+#define GraphTraversal_H
+
+#include "Graph.h"
+#include "LinkedQueue.h"
+#include <stdio.h>
+
+void DFS(Vertex* V);
+void BFS(Vertex* V, LinkedQueue* Queue);
+
+#endif /* GraphTraversal_h */
---
+++ algorithm_xcode/hamster/GraphTraversal/LinkedQueue.h
@@ -0,0 +1,30 @@
+#ifndef LINKED_QUEUE_H
+#define LINKED_QUEUE_H
+
+#include "Graph.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct tagNode {
+    Vertex* Data;
+    struct tagNode* NextNode;
+}Node;
+
+typedef struct tagLinkedQueue {
+    Node* Front;
+    Node* Rear;
+    int Count;
+}LinkedQueue;
+
+LinkedQueue* LQ_CreateQueue();
+void LQ_DestroyQueue(LinkedQueue* Queue);
+
+Node* LQ_CreateNode(Vertex* NewData);
+void LQ_DestroyNode(Node* _Node);
+void LQ_Enqueue(LinkedQueue* Queue, Node* NewNode);
+Node* LQ_Dequeue(LinkedQueue* Queue);
+
+int LQ_IsEmpty(LinkedQueue* Queue);
+
+#endif
