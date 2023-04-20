f6623f9e748f97a542cb311965444e5a0db109d4
HamsterStudent <hwanyamyu@gmail.com>
2023-04-20 18:33:48 +0900

    MST

---
+++ algorithm_xcode/hamster/MinimumSpanningTree/MST.h
@@ -0,0 +1,14 @@
+
+#ifndef MST_h
+#define MST_h
+
+#include "Graph.h"
+#include "PriorityQueue.h"
+#include "DisjointSet.h"
+
+#define MAX_WEIGHT 36267
+
+void Prim(Graph* G, Vertex* StartVertex, Graph* MST);
+void Kruskal(Graph* G, Graph* MST);
+
+#endif /* MST_h */
